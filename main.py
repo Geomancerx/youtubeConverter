26bfad5c088e4c79286e395ce14e1631b45d9809
geomancerx <geomancerx@yahoo.com>
2023-04-20 18:14:25 -0700

    more fixes to gui file

--- main.py
+++ main.py
@@ -1,85 +1,104 @@
 import os
 import sys
-from pytube import YouTube
-from moviepy.editor import *
+import youtube_dl
 
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QFileDialog, QRadioButton
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QRadioButton, QFileDialog, QMessageBox
+from PyQt5 import QtGui
+
 
 class VideoConverterGUI(QWidget):
     def __init__(self):
         super().__init__()
+
         self.initUI()
 
     def initUI(self):
-        self.url_label = QLabel("Enter YouTube video URL:")
-        self.url_entry = QLineEdit()
-        self.format_label = QLabel("Select output format:")
-        self.mp4_radio = QRadioButton("MP4")
-        self.mp3_radio = QRadioButton("MP3")
-        self.output_label = QLabel("Enter output file name:")
-        self.output_entry = QLineEdit()
-        self.download_button = QPushButton("Download and Convert")
-        self.browse_button = QPushButton("Browse")
+        self.url_label = QLabel("YouTube URL:", self)
+        self.url_label.move(20, 20)
+        self.url_label.setFixedWidth(105)
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.url_label)
-        vbox.addWidget(self.url_entry)
-        vbox.addWidget(self.format_label)
-        vbox.addWidget(self.mp4_radio)
-        vbox.addWidget(self.mp3_radio)
-        vbox.addWidget(self.output_label)
-        vbox.addWidget(self.output_entry)
-        vbox.addWidget(self.browse_button)
-        vbox.addWidget(self.download_button)
+        self.url_entry = QLineEdit(self)
+        self.url_entry.move(110, 20)
+        self.url_entry.resize(270, 20)
 
-        self.setLayout(vbox)
+        self.output_label = QLabel("Output file path:", self)
+        self.output_label.move(20, 60)
+        self.output_label.setFixedWidth(105)
 
-        self.download_button.clicked.connect(self.download_and_convert_video)
-        self.browse_button.clicked.connect(self.browse_output_directory)
+        self.output_entry = QLineEdit(self)
+        self.output_entry.move(110, 60)
+        self.output_entry.resize(270, 20)
 
-        self.setWindowTitle("YouTube Video Converter")
+        self.browse_button = QPushButton("Browse", self)
+        self.browse_button.move(500, 60)
+        self.browse_button.resize(80, 20)
+        self.browse_button.clicked.connect(self.browse_file_path)
+
+        self.mp3_radio = QRadioButton("MP3", self)
+        self.mp3_radio.move(110, 100)
+
+        self.mp4_radio = QRadioButton("MP4", self)
+        self.mp4_radio.move(190, 100)
+        self.mp4_radio.setChecked(True)
+
+        self.convert_button = QPushButton("Convert", self)
+        self.convert_button.move(20, 140)
+        self.convert_button.clicked.connect(self.download_and_convert_video)
+
+        # Set font size and color
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+
+        self.url_label.setFont(font)
+        self.url_entry.setFont(font)
+        self.output_label.setFont(font)
+        self.output_entry.setFont(font)
+        self.browse_button.setFont(font)
+
+        self.url_entry.setStyleSheet("color: black;")
+        self.output_entry.setStyleSheet("color: black;")
+
+        # Set horizontal spacing between widgets
+        self.url_entry.move(110 + self.url_label.width() + 10, 20)
+        self.output_label.move(20, 60)
+        self.output_entry.move(110 + self.output_label.width() + 10, 60)
+
+        self.resize(900, 300)
+        self.setWindowTitle("YouTube Converter")
         self.show()
 
-    def browse_output_directory(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.ShowDirsOnly
-        output_directory = QFileDialog.getExistingDirectory(self, "Select Output Directory", options=options)
-        self.output_entry.setText(output_directory + "/output")
+    def browse_file_path(self):
+        file_name, _ = QFileDialog.getSaveFileName(self, "Select output file", os.path.expanduser("~"), "*.mp3;;*.mp4")
+        if file_name:
+            self.output_entry.setText(file_name)
 
     def download_and_convert_video(self):
         url = self.url_entry.text()
-        if not url:
-            print("Please enter a YouTube video URL.")
-            return
+        output_file = self.output_entry.text()
+        extension = "mp3" if self.mp3_radio.isChecked() else "mp4"
 
-        if self.mp4_radio.isChecked():
-            extension = "mp4"
-            codec = "libx264"
-        elif self.mp3_radio.isChecked():
-            extension = "mp3"
-            codec = None
+        ydl_opts = {
+            'outtmpl': output_file + '.' + extension,
+        }
+
+        if extension == "mp3":
+            ydl_opts['format'] = 'bestaudio/best'
+            ydl_opts['postprocessors'] = [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }]
         else:
-            print("Please select an output format.")
-            return
+            ydl_opts['format'] = 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4'
 
-        output_file_path = self.output_entry.text()
-        if not output_file_path:
-            print("Please enter an output file name.")
-            return
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
 
-        try:
-            yt = YouTube(url)
-            video = yt.streams.first()
-            video.download()
+        QMessageBox.information(self, "Download Complete", "Video conversion complete!")
 
-            clip = VideoFileClip(video.default_filename)
-            clip.write_videofile(output_file_path + '.' + extension, codec=codec)
 
-            print("Video conversion complete!")
-        except Exception as e:
-            print("An error occurred while converting the video:", e)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = VideoConverterGUI()
     sys.exit(app.exec_())
